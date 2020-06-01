f518f8de50b3af5412c267f8867f40f19fe1ce04
Steffen Fehrmann <steffenfehrmann@gmx.net>
2020-06-02 00:19:09 +0200

    added variance subsettingand heatmap plotting

--- src/data_analysis.r
+++ src/data_analysis.r
@@ -9,6 +9,8 @@ library(dplyr)
 library(visdat) # required for vis_miss
 library(tibble)
 library(limma)
+library(RColorBrewer)
+library(pheatmap)
 
 source('src/helper_functions.R')
 
@@ -18,11 +20,12 @@ prot_exp_raw <- read_excel("data_052020/Protein_Exp__FeatureLevel_artificial.xls
                        col_names = TRUE,
                        na = "NA")
 
-# split feature pattern and sum by protein
+# split feature pattern and sum by protein, ungroup after operation
 prot_exp <- prot_exp_raw %>%
   separate(Feature, c('Protein', 'Peptide', 'Charge'), sep = '_', remove = TRUE) %>%
-    group_by(Protein) %>%
-    summarise_at(vars(Pool1_126:Pool2_131N), sum, na.rm = TRUE)
+  group_by(Protein) %>%
+  summarise_at(vars(Pool1_126:Pool2_131N), sum, na.rm = TRUE) %>%
+  ungroup()
 
 # visualization of missing values
 vis_miss(prot_exp[,2:20]) # no missing values after summa
@@ -89,5 +92,42 @@ tr_prot_exp_cor_annotated <- prot_exp_cor %>%
 # compute and plot PCA
 my_scaled_pca <- prcomp(tr_prot_exp_cor_annotated[8:ncol(tr_prot_exp_cor_annotated)], scale. = TRUE)
 autoplot(my_scaled_pca, data=tr_prot_exp_cor_annotated, colour = 'Condition', shape = 'RepBinary', scale = 0)
-# possible misslabeling of sample 17
+# possible mislabeling of sample 17 (Rep3/Cond3)
 autoplot(my_scaled_pca, data=tr_prot_exp_cor_annotated, colour = 'Condition', shape = 'BioRep', scale = 0)
+
+# compute variance and keep top 2% only
+prot_exp_cor_top2perc <- prot_exp_cor %>%
+  rowwise() %>%
+  mutate(variance = var(c_across(starts_with("Pool")))) %>%
+  ungroup() %>%
+  slice_max(variance, prop = 0.02)
+
+# check dependence mean / variance
+prot_exp_cor_mnvar <- prot_exp_cor %>%
+  rowwise() %>%
+  mutate(variance = var(c_across(starts_with("Pool"))),
+         mean = mean(c_across(starts_with("Pool"))),
+         median = median(c_across(starts_with("Pool"))))
+
+ggplot(prot_exp_cor_mnvar, aes(x=median, y= variance)) +
+  geom_point() +
+  scale_x_continuous(trans = 'log10') +
+  scale_y_continuous(trans = 'log10') +
+  geom_smooth(method=lm)
+
+prot_exp_matrix_cor_top2perc <- as.matrix(prot_exp_cor_top2perc[2:20])
+rownames(prot_exp_matrix_cor_top2perc) <- prot_exp_cor_top2perc$Protein
+colnames(prot_exp_matrix_cor_top2perc) <- paste(sample_annotation$Condition, '/', sample_annotation$BioRep, sep='')
+color_vec <- colorRampPalette(brewer.pal(9, "YlOrRd"))(255)
+color_vec <- colorRampPalette(brewer.pal(9, "Blues"))(255)
+# as expected, not very expressive
+pheatmap(prot_exp_matrix_cor_top2perc,col=color_vec)
+
+# two values are less than 0, replace with a values slightly lower than the next lowest
+prot_exp_matrix_cor_top2perc[prot_exp_matrix_cor_top2perc<0] <- 6
+pheatmap(log10(prot_exp_matrix_cor_top2perc),col=color_vec)
+
+ggplot(prot_exp_cor, aes(x=Pool1_126, y=Pool2_130N)) +
+  geom_point() +
+  scale_x_continuous(trans = 'log10') +
+  scale_y_continuous(trans = 'log10')
